614757d9452fff1b951121f316cad3fd6cea12be
Carlos Andrés Florez <caflorez@uniquindio.edu.co>
2021-04-30 17:04:57 -0500

    Comenzamos la capa de presentación web

---
+++ web/src/main/java/co/edu/uniquindio/proyecto/bean/InicioBean.java
@@ -0,0 +1,20 @@
+package co.edu.uniquindio.proyecto.bean;
+
+import org.springframework.stereotype.Component;
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+
+@Component
+@ViewScoped
+public class InicioBean implements Serializable {
+
+    private String mensaje = "Hola mundo desde InicioBean!";
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
